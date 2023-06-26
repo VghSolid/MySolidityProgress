fd91e1d39b08220098ee11e44624d0a518e576a9
VghSolid <v.ghodsi71@gmail.com>
2023-06-26 18:09:39 +0330

    Q&A update

--- Token_SafeMath.sol
+++ Token_SafeMath.sol
@@ -3,6 +3,7 @@ pragma solidity >0.7.0 <0.9.0;
 
 library SafeMath{
     //why do we need the Requier?
+    //update: because of overflow(or underflow problem): if C becomes greater than max number(256bit) or lesser than min. 
     function Add(uint256 a,uint256 b) internal pure returns (uint256) {
         uint256 c = a+b;
         require(c >= a, "SafeMath: add overflow");
@@ -10,11 +11,11 @@ library SafeMath{
     }
     //okay
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-       require(a >= b,"Error");
+       require(a >= b,"SafeMath: add overflow");
        return a-b;
     }
     //why do we need the Requier? 
-    //why do we seprate a==0 from others?
+    //why do we seprate a==0 from others? update: because the denominator in the requier section can't be zero.
     function multiply(uint256 a,uint256 b) internal pure returns(uint256){
         if(a==0) {
             return 0;
