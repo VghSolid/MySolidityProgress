7756f60dbed6b992c543ae8da192e12b7b43f6db
VghSolid <v.ghodsi71@gmail.com>
2023-07-13 15:13:47 +0330

    Progress

--- ERC20_New.sol
+++ ERC20_New.sol
@@ -76,7 +76,9 @@ abstract contract ERC20_new is context,IERC20, IERC20Errors {
     function _update(address from, address to, uint256 amount) internal virtual {
         if(from == address(0)){
             _totalsupply += amount;
+
             }else{
+
             uint256 fromBalance = _balance[from];
             if(fromBalance < amount){
                 revert ERC20InsufficientBalance(from, fromBalance, amount);
@@ -92,7 +94,9 @@ abstract contract ERC20_new is context,IERC20, IERC20Errors {
                 _totalsupply -= amount;
 
             }
+
           }else{
+
             unchecked {
                 _balance[to] += amount;
                 
@@ -100,5 +104,51 @@ abstract contract ERC20_new is context,IERC20, IERC20Errors {
         }
         emit Transfer(from, to, amount);
 
+    }
+
+    function allowance(address owner,address spender) public virtual view returns (uint256) {
+        return _allowance[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) public virtual returns(bool){
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal virtual{
+        _approve(owner, spender, value, true);
+    }
+    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
+        if(owner == address(0)){
+            revert ERC20InvalidApprover(address(0));
+        }
+        if(spender == address(0)){
+            revert ERC20InvalidSpender(address(0));
+        }
+        _allowance[owner][spender] = value;
+        if(emitEvent == true){
+            emit Approval(owner, spender, value);
+        }
+
+
+    }
+
+    function transferfrom(address sender, address recipient, uint256 amount) public virtual returns(bool) {
+        address spender = _msgSender();
+        _spendAllowance(sender, spender, amount);
+        _transfer(sender, recipient, amount);
+    }
+
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if(currentAllowance != type(uint256).max){
+            if(currentAllowance < value) {
+                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
+            }
+            unchecked {
+                _approve(owner, spender, currentAllowance-value, false);
+            }
+        }
     }
 }
